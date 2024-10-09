ec20144caf62f149b9a74db5a19068d88da09720
jalvarez1119 <159866836+jalvarez1119@users.noreply.github.com>
2024-10-08 19:11:51 -0600

    esto es una nueva prueba

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,4 @@
 console.log('hola mundo')
-este es un ejercicio de uso de git y github
+//este es un ejercicio de uso de git y github
 console.log("joseph alvarez sandoval")
+hola esto es una prueba
